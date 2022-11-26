9aff7d307ecf1d614786aaf14459bedc2780b761
SurajPatil8252 <115831542+SurajPatil8252@users.noreply.github.com>
2022-11-26 16:48:08 +0530

    all files added

---
+++ src/javaConditionaStatementsAndLoops/Example1.java
@@ -0,0 +1,25 @@
+package javaConditionaStatementsAndLoops;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 15;
+		
+		System.out.println(a%2==0);
+		
+		if(a%2==0)
+		{
+			System.out.println("number is even");
+		
+		}
+		else
+		{
+			System.out.println("number is odd");
+		}
+
+	}
+			
+
+	}
+
